0b82947a85d8844bc560a7514fcca69494c95f6f
Russell <russell@baidu.com>
2019-11-07 17:00:33 +0800

    修复排序名称为空时错误的问题

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import sortzh from './sortzh'
 function switchkey (obj, sortName, sortName2) {
   let r
   let witch = obj[sortName]?sortName:sortName2
+  if (!witch) {
+    return ''
+  }
   if (/^[a-zA-Z]/.test(obj[witch] ? obj[witch] : '没有')) {
     r = obj.en2zh
   } else {
